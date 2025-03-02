668257d4c9f5b91bf472953015e6ed28fc586cf9
Jimmy Shura <jshura@stu.southtexascollege.edu>
2025-03-02 16:55:50 -0600

    Chnaged Image to City

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -12,7 +12,7 @@
 <template>
     <RouterLink :to="{name: 'CardDetail', params: {id: username}}">
         <div class="border border-lime-400 rounded-lg shadow bg-gray-400 h-full">
-            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'birds', height: 480, width: 960})" />
+            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'city', height: 480, width: 960})" />
             <div class="p-4">
                 <p class="mb-2 text-2xl text-cyan-400 font-bold">{{ firstname }} {{ lastname }}</p>
                 <p class="mb-3 font-normal text-green-600">{{ jobtitle }}</p>
